87a9bfdca74f14455fed76a9ce58f320a2a1924c
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2025-02-13 21:48:10 -0300

    feat: Adicionado validacao de email ja existente

--- lib/domain/usecases/autenticar_usuario.dart
+++ lib/domain/usecases/autenticar_usuario.dart
@@ -1,18 +1,32 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AutenticarUsuario {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  final FirebaseAuth _firebaseAuth =  FirebaseAuth.instance;
-  
-  entrarUsuario({required String email, required String senha}){
+  entrarUsuario({required String email, required String senha}) {}
 
+  Future <String?> cadastrarUsuario(
+      {required String email,
+      required String senha,
+      required String nome}) async {
 
+  try {
+      UserCredential userCredential = 
+          await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, 
+        password: senha,
+      );
 
+      await userCredential.user!.updateDisplayName(nome);
+      print("Funcionou! Chegamos até essa linha!");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "email-already-in-use":
+          return "O e-mail já está em uso.";
+      }
+      return e.code;
+    }
+
+    return null;
   }
-
-cadastrarUsuario({required String email, required String senha, required String nome}){
-
-
-}
-  
 }
